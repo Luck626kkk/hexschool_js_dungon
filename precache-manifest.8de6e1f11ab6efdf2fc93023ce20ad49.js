033eda86b08a3ec1a49fcde2159ed41b2339a598
Luck <aaa26905677a@gmail.com>
2019-02-07 20:48:25 +0800

    Updates

---
+++ precache-manifest.8de6e1f11ab6efdf2fc93023ce20ad49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hexschool_js_dungon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "94e6180837986e173920",
+    "url": "/hexschool_js_dungon/static/js/runtime~main.94e61808.js"
+  },
+  {
+    "revision": "008f4361473bacb4d6c2",
+    "url": "/hexschool_js_dungon/static/js/main.008f4361.chunk.js"
+  },
+  {
+    "revision": "b4134f68b5f78f10fbfd",
+    "url": "/hexschool_js_dungon/static/js/1.b4134f68.chunk.js"
+  },
+  {
+    "revision": "008f4361473bacb4d6c2",
+    "url": "/hexschool_js_dungon/static/css/main.5a1cfcbb.chunk.css"
+  },
+  {
+    "revision": "0ed962e051194803767f5f5246a5403d",
+    "url": "/hexschool_js_dungon/index.html"
+  }
+];
